fc1cac52cc19d60c392bf3d6f078f4bc94b10d2f
amcknight <amcknight@relaynetwork.com>
2019-06-04 09:19:55 -0400

    add component files

---
+++ src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-book-detail',
+  templateUrl: './book-detail.component.html',
+  styleUrls: ['./book-detail.component.css']
+})
+export class BookDetailComponent implements OnInit {
+  @Input() book: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/service/product.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "./base.service";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService  extends BaseService{
+  constructor(http: HttpClient){super("products",http)}
+}
