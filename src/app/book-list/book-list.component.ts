59a63864c51f4c833d864cbb140fc1b0dca9ff5f
amcknight <amcknight@relaynetwork.com>
2019-06-05 08:20:43 -0400

    create a more refined search

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -19,7 +19,20 @@ export class BookListComponent implements OnInit {
   list(){
      let query:any = {};
      if(this.searchText){
-       query['title'] =  { '$regex': this.searchText + ".*"};
+
+         let title_query = {};
+         let author_query = {};
+         let isbn_query = {};
+
+         title_query['title'] = {'$regex': this.searchText + ".*", '$options' : 'i'};   //case insensitive title starts with
+         author_query['author'] = {'$regex': this.searchText + ".*",'$options' : 'i'};  //case insensitive author starts with
+         isbn_query['isbn'] = {'$regex': this.searchText + ".*"};                       //case sensitive isbn starts with
+
+         query['$or'] = []; //or the queries
+         query['$or'].push(title_query);
+         query['$or'].push(author_query);
+         query['$or'].push(isbn_query);
+         //query['title'] =  { '$regex': this.searchText + ".*"};
      }
     this.bs.list(query).subscribe((data)=>{
             this.books = data.rows;
